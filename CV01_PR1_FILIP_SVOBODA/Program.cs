6a4cedd0cea9c9a7224b4ec23910b2a457a16a94
Filip Svoboda <st58611@upce.cz>
2021-02-24 22:21:46 +0100

    Přidejte soubory projektu.

---
+++ CV01_PR1_FILIP_SVOBODA/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace CV01_PR1_FILIP_SVOBODA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Josef Novák");
+            Console.WriteLine("Jindřišská 16");
+            Console.WriteLine("111 50, Praha  1");
+        }
+    }
+}
